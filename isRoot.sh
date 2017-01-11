1885609e5f52ee1c0bb262c1c902df6e38cc6a13
Kevin Weis <Kevin.Weis@plan.de>
2017-01-11 18:55:52 +0100

    update isRoot to show help
    when directly calling script with any parameter

--- isRoot.sh
+++ isRoot.sh
@@ -6,15 +6,15 @@ Return "true" if user is root, else return "false".
 EOF
 }
 
+if [ $# -ne 0 ]; then
+    printHelp;
+    return 1;
+fi
+
 function isRoot {
-	if [ $# -ne 0 ]; then
-		printHelp;
-		return 1;
-	fi
+    if [ ${EUID} == 0 ]; then
+        echo "true";
+    fi
 
-	if [ ${EUID} == 0 ]; then
-		echo "true";
-	fi
-
-	echo "false";
+    echo "false";
 }
