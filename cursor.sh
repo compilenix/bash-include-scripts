df5163deeec605549dababc64ee6e658184c69db
Kevin Weis <Kevin.Weis@plan.de>
2017-01-11 18:54:29 +0100

    update cursor.sh
    - adding help
    - swap return array value order to 0 -> x and 1 -> y.

--- cursor.sh
+++ cursor.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
-# example, to save current cursor position in pos1, use:
-# extract_current_cursor_position pos1;
-# To see cursor positions saved in pos1, you can use:
-# echo ${pos1[0]} ${pos1[1]};
-# To move/restore cursor position to pos1, you have to use:
-# tput cup ${pos1[0]} ${pos1[1]};
+function printHelp {
+cat << EOF
+  extract_current_cursor_position       - first parameter used as destination variable
+  Usage:
+    example, to save current cursor position in pos1
+    # extract_current_cursor_position pos1;
+    To see cursor positions saved in pos1
+    # echo ${pos1[0]} ${pos1[1]};
+    To move/restore cursor position to pos1
+    # tput cup ${pos1[0]} ${pos1[1]};
+EOF
+}
+
+if [ $# -ne 0 ]; then
+    printHelp;
+    return 1;
+fi
+
 function extract_current_cursor_position {
     export $1;
     exec < /dev/tty;
@@ -14,6 +26,6 @@ function extract_current_cursor_position {
     echo -en "\033[6n" > /dev/tty;
     IFS=';' read -r -d R -a pos;
     stty $oldstty;
-    eval "$1[0]=$((${pos[0]:2} - 2))";
-    eval "$1[1]=$((${pos[1]} - 1))";
+    eval "$1[0]=$((${pos[1]} - 1))";
+    eval "$1[1]=$((${pos[0]:2} - 2))";
 }
