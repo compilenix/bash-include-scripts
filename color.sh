bef26b73391343077334a2b989c0e05ae2893418
Kevin Weis <Kevin.Weis@plan.de>
2017-01-11 17:49:57 +0100

    Add color.sh

---
+++ color.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+color_reset='\e[0m';
+
+# Regular
+color_black='\e[0;30m';		# Black
+color_red='\e[0;31m';		# Red
+color_green='\e[0;32m';		# Green
+color_yellow='\e[0;33m';	# Yellow
+color_blue='\e[0;34m';		# Blue
+color_purple='\e[0;35m';	# Purple
+color_cyan='\e[0;36m';		# Cyan
+color_white='\e[0;37m';		# White
+
+# Bold
+color_bblack='\e[1;30m';	# Bold Black
+color_bred='\e[1;31m';		# Bold Red
+color_bgreen='\e[1;32m';	# Bold Green
+color_byellow='\e[1;33m';	# Bold Yellow
+color_bblue='\e[1;34m';		# Bold Blue
+color_bpurple='\e[1;35m';	# Bold Purple
+color_bcyan='\e[1;36m';		# Bold Cyan
+color_bwhite='\e[1;37m';	# Bold White
